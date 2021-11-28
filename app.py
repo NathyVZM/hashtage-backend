18503f64501bb8f96e8cc0d2e83db61ad3407a53
Nathalie <nathalievzm@hotmail.com>
2021-11-28 19:46:13 -0400

    testing heroku deploy

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    print('Hello World')
+
 app.config.from_object(DevelopmentConfig)
 app.secret_key = os.environ['SECRET_KEY']
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1000 * 1000 # 20MB
