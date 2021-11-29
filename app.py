67dc8bec8deb53514508864f51e510e2e0d9cd64
Nathalie <nathalievzm@hotmail.com>
2021-11-28 21:03:55 -0400

    enable cors

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# app.py
+
 from flask import Flask, session
 from flask_mongoengine import MongoEngine
+from flask_cors import CORS
 from config import *
 import os
 
@@ -12,8 +15,12 @@ def home():
 app.config.from_object(ProductionConfig)
 app.secret_key = os.environ['SECRET_KEY']
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1000 * 1000 # 20MB
-app.config['MONGODB_SETTINGS'] = {
-    'db': os.environ['MONGODB_DB'],
-    'host': os.environ['MONGODB_HOST']
-}
+
+
+# MongoEngine
+app.config['MONGODB_SETTINGS'] = { 'db': os.environ['MONGODB_DB'], 'host': os.environ['MONGODB_HOST'] }
 db = MongoEngine(app)
+
+
+# Enabling CORS
+cors = CORS(app, supports_credentials=True)
