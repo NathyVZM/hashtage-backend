6a787df0178be67ddebfe6462f2cf64d0e62cd22
Nathalie <nathalievzm@hotmail.com>
2021-11-28 20:50:48 -0400

    configure mongodb database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from flask_mongoengine import MongoEngine
 from config import *
 import os
 
@@ -11,3 +12,8 @@ def home():
 app.config.from_object(ProductionConfig)
 app.secret_key = os.environ['SECRET_KEY']
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1000 * 1000 # 20MB
+app.config['MONGODB_SETTINGS'] = {
+    'db': os.environ['MONGODB_DB'],
+    'host': os.environ['MONGODB_HOST']
+}
+db = MongoEngine(app)
